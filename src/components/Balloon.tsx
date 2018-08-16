8c7fc50c945033bc69cc613f99285dc8132a22a2
i5irin <mail@isirin.com>
2018-08-16 21:30:20 +0900

    吹き出しのコンポーネントを追加

--- src/components/Balloon.tsx
+++ src/components/Balloon.tsx
@@ -1,7 +1,29 @@
 import * as React from 'react';
+import * as styles from './Balloon.css';
+import classNameProps from '../class-name-props';
 
-const balloon: React.StatelessComponent = () => {
-  return <div />;
+interface IBalloonProps {
+  // TODO: スタンプに対応したいけど、イラスト……
+  message: string;
+  isYourMessage: boolean;
+}
+
+const balloon: React.StatelessComponent<
+  IBalloonProps & classNameProps
+> = props => {
+  const { message, isYourMessage, className } = props;
+  const classes = `${className} ${styles.balloon} ${
+    // FIXME: 文字列出しちゃったら型意味なくない？
+    isYourMessage ? styles['balloon--yours'] : styles['balloon--hers']
+  }`;
+  return (
+    <div>
+      <div className={classes}>
+        {/* FIXME: 文字列出しちゃったら型意味なくない？ */}
+        <p className={styles['balloon__message']}>{message}</p>
+      </div>
+    </div>
+  );
 };
 
 export default balloon;
