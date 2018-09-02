8dfce6c9f06858d4ffd60de237d198dbb536d579
i5irin <mail@isirin.com>
2018-09-02 17:57:24 +0900

    メッセージ入力用コンポーネントの送信ボタン押下とメッセージ送信Actionを紐付ける

--- src/Wave.tsx
+++ src/Wave.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react';
 import MessageLog from './components/MessageLog';
 import MessengerHeader from './components/MessengerHeader';
+import MessageInput from './components/MessageInput';
+import { Dispatch, bindActionCreators } from 'redux';
+import { ISendMessageAction } from './actions/mesages';
+import { connect } from 'react-redux';
+// tslint:disable-next-line:no-duplicate-imports
+import * as MessageActions from './actions/mesages';
 
-const wave: React.StatelessComponent = () => {
+interface IWaveProps {
+  sendMessage: (text: string) => ISendMessageAction;
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(
+    {
+      sendMessage: MessageActions.sendMessage
+    },
+    dispatch
+  );
+
+const wave: React.StatelessComponent<
+  IWaveProps & { className?: string }
+> = props => {
   return (
     <div>
       <MessengerHeader />
       <MessageLog />
+      <MessageInput sendMessage={props.sendMessage} />
     </div>
   );
 };
 
-export default wave;
+export default connect(
+  null,
+  mapDispatchToProps
+)(wave);
