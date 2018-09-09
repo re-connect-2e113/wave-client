4f8ea35a56fb6af27c62baf0e0149ceb22a84166
i5irin <mail@isirin.com>
2018-09-10 00:44:07 +0900

    貴方のメッセージの見た目変更

--- src/components/YourMessage.tsx
+++ src/components/YourMessage.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import Balloon from './Balloon';
 import * as styles from './YourMessage.css';
+import classNameProps from '../class-name-props';
+import utils from '../utils';
 
 interface IYourMessageProps {
   message: string;
 }
 
-const yourMessage: React.StatelessComponent<IYourMessageProps> = props => {
+const yourMessage: React.StatelessComponent<
+  IYourMessageProps & classNameProps
+> = props => {
   return (
-    <div className={styles.root}>
+    <div className={`${utils.toStringOrEmpty(props.className)} ${styles.root}`}>
       <div>
         <p className={styles.yourMessageActionLog}>YOU WROTE...</p>
         <Balloon fromLeft={false} message={props.message} />
