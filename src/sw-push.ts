417d8a33e09f9780d70a6c1cccbfc2e54800f014
i5irin <mail@isirin.com>
2018-09-15 04:24:41 +0900

    WebPush通知を受け取ったらUIに伝えるよう更新

--- src/sw-push.ts
+++ src/sw-push.ts
@@ -1,13 +1,20 @@
-self.addEventListener('push', event => {
-  const serviceWorkerEvent: any = event;
-  const data = serviceWorkerEvent.data.json();
+// TODO: 大きくなってきたら型定義使うためにServiceWorker関係は別リポジトリへ
+// (tsconfigのdomとwebworkerは共存できない)
+self.addEventListener('push', e => {
+  const event: any = e;
+  if (event.data === null) {
+    return;
+  }
+  const data = event.data.json();
   const title = data.title;
   const body = {
     body: data.body,
     icon: data.icon
   };
-  const worker: any = self;
-  serviceWorkerEvent.waitUntil(
-    worker.registration.showNotification(title, body)
-  );
+  const s: any = self;
+  s.clients.matchAll().then((clients: any) => {
+    clients.forEach((client: any) =>
+      client.postMessage({ title, icon: body.icon, body: data.body })
+    );
+  });
 });
